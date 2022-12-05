c6496389bba1dd7627dd5060afb4a7cd8f61439f
Murilo Beltrame <murilobeltrame@gmail.com>
2022-12-05 10:23:11 -0300

    ....

---
+++ src/Wineyard.Tools/Migrations/20221205132137_Init.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wineyard.Tools.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Wines",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    WineryName = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Label = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    CountryName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Vintage = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wines", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grapes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    WineId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grapes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Grapes_Wines_WineId",
+                        column: x => x.WineId,
+                        principalTable: "Wines",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grapes_WineId",
+                table: "Grapes",
+                column: "WineId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Grapes");
+
+            migrationBuilder.DropTable(
+                name: "Wines");
+        }
+    }
+}
